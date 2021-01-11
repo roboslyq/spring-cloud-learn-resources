1c0d79a855f2753ae00537dfd418cfa5473818d1
luo.yongqian <luo.yongqian@yuexiu-finance.com>
2021-01-11 17:59:29 +0800

    updated

--- spring-cloud-learn/spring-cloud-server-demo/src/main/java/com/roboslyq/springcloudeurekaclient/ServiceProviderApplication.java
+++ spring-cloud-learn/spring-cloud-server-demo/src/main/java/com/roboslyq/springcloudeurekaclient/ServiceProviderApplication.java
@@ -3,15 +3,17 @@ package com.roboslyq.springcloudeurekaclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-//启用服务发瑞客户端
-@EnableDiscoveryClient
-@RestController
+@SpringCloudApplication
+//@SpringBootApplication
+////启用服务发瑞客户端
+//@EnableDiscoveryClient
+//@RestController
 public class ServiceProviderApplication {
 	@Value("${server.port}")
 	private int serverPort;
