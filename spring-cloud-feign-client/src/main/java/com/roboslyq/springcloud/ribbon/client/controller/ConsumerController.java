1baf41ffe586b9f23c6777b7c68544ad160f858e
roboslyq <robosloveu@163.com>
2021-02-27 18:45:49 +0800

    refactor

--- spring-cloud-feign-client/src/main/java/com/roboslyq/springcloud/ribbon/client/controller/ConsumerController.java
+++ spring-cloud-feign-client/src/main/java/com/roboslyq/springcloud/ribbon/client/controller/ConsumerController.java
@@ -1,10 +1,9 @@
 package com.roboslyq.springcloud.ribbon.client.controller;
 
-import com.roboslyq.springcloud.ribbon.client.service.FeignServiceDemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.roboslyq.springcloud.ribbon.client.service.FeignServiceDemo;
 @RestController
 public class ConsumerController {
     //注入接口
